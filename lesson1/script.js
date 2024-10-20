c972a9ef4f8697cbfd8dc5af7165a5ac460dc7eb
Ramin <ramin_n11@icloud.com>
2024-10-21 01:45:19 +0300

    Did first homework

---
+++ lesson1/script.js
@@ -0,0 +1,12 @@
+// Задание 1
+// let degCelsius = 25;
+// let degFaringheit = (9/5 + degCelsius) + 32;
+// console.log(`Температура в градусах Цельсия: ${degCelsius}`);
+// console.log(`Температура в градусах Фаренгейта: ${degFaringheit}`);
+
+
+// Задание 2
+
+let myName= 'Ramin';
+let admin = myName;
+console.log(`Значение переменной admin: ${admin}`);
