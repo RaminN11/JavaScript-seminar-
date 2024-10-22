de1d229edb7a472749ce100d0a716eab2a731405
Ramin <ramin_n11@icloud.com>
2024-10-22 14:44:47 +0300

    Did third seminar  for JavaScript

---
+++ lesson3/function.js
@@ -0,0 +1,114 @@
+//Задание 1
+
+
+// function allUsersInfo(name, surname, age) {
+//     console.log(`Привет ${name} ${surname} с возрастом ${age}`);
+// }
+
+// allUsersInfo('Иван', 'Петров', 30);
+
+// function pow(num) {
+//     console.log(num ** 2);
+// }
+
+// pow(3);
+
+// function positiveNegative(num) {
+//     if (num >= 0) {
+//         console.log('+++');
+//     } else {
+//         console.log('---');
+//     }
+// }
+// positiveNegative(3);
+// positiveNegative(-3);
+
+
+//Задание 2
+
+
+// function sum(a, b, c) {
+//     console.log(a + b + c);
+// }
+
+// let param1 = 1;
+// let param2 = 2;
+// let param3 = 3;
+
+// sum(param1, param2, param3);
+
+//Задание 3
+
+// function square(num) {
+//     return Math.sqrt(num);
+// }
+
+// console.log(square(3));
+
+// function minValue(num1, num2) {
+//     if (num1 < num2) {
+//         console.log('Первое число наименьшее');
+//     } else {
+//         console.log('Второе число наименьшее');
+        
+//     }
+// }
+
+// minValue(5, 3);
+// minValue(3,5)
+
+//Задание 4
+
+// function dayOfWeek(day) {
+//     switch (day) {
+//         case 1:
+//             return 'Понедельник';        
+//             break;
+
+//         case 2:
+//             return 'Вторник';        
+//             break;
+
+//         case 3:
+//             return 'Среда';        
+//             break;
+
+//         case 4:
+//             return 'Четверг';        
+//             break;
+
+//         case 5:
+//             return 'Пятница';        
+//             break;
+
+//         case 6:
+//             return 'Суббота';        
+//             break;
+
+//         case 7:
+//             return 'Воскресенье';        
+//             break;
+
+//         default: return "Число не от 1 до 7"
+//             break;
+//     }
+// }
+// console.log(dayOfWeek(4));
+// console.log(dayOfWeek(8));
+
+// function sayHello(time, name) {
+//     if (time >= 4 && time <= 11) {
+//         console.log(`Доброе утро ${name}`);
+//     } else if (time >= 12 && time <= 16) {
+//         console.log(`Добрый день ${name}`);
+//     } else if (time >= 17 && time <= 23) {
+//         console.log(`Добрый вечер ${name}`);
+//     } else {
+//         console.log(`Доброй ночи ${name}`);
+//     }
+// }
+
+// sayHello(17, 'John');
+// sayHello(10, 'John');
+// sayHello(15, 'John');
+// sayHello(1, 'John');
